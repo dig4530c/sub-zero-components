fb58da6314cfa2f0c3256e3406d6a716235a6398
Edgardo Jimenez <edjimenez87@gmail.com>
2013-10-30 18:59:57 -0400

    Merging...

--- is/client_sidebar.php
+++ is/client_sidebar.php
@@ -1,4 +1,4 @@
-<div id="sidebar">
+<div id="sidebar" class="user">
 	<div><h3>Account Home</h3></div>
 	<div>
 		<ul>
@@ -6,15 +6,6 @@
 			<li><a href="change_user_info.php" class="changeuser">Change User Info</a></li>
 			<li><a href="change_password.php" class="changepass">Change Password</a></li>
 			<li><a href="admin.php" >Admin. Panel</a></li>
-		</ul>
-	</div>
-	<div>
-		<h3>Orders</h3>
-	</div>
-	<div>
-		<ul>
-			<li><a href="#" class="order">Track Orders</a></li>
-			<li><a href="#" class="orderh">Order History</a></li>
 		</ul>
 	</div>
 </div>
