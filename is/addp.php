a480fc208d7961b053e4573d52c9c7fd7f9248a9
recked <flipmodes01@gmail.com>
2013-11-07 16:12:18 -0500

    Slight changes to admin functions

--- is/addp.php
+++ is/addp.php
@@ -1,6 +1,23 @@
+<?php
+
+if(isset($_GET['action'])=='processp') {
+	echo "<script type='text/javascript'>
+		//$(document).ready(function() {
+			//$('#mmake').show();
+			//$('#superuser').hide();
+			
+			//});
+			</script>
+			
+			";
+    processp();
+}else
+//show form
+?>
+
 
 <div id='addp'>
-<form enctype="multipart/form-data" action="is/processp.php" method="post" accept-charset="utf-8">
+<form enctype="multipart/form-data" action="?action=processp" method="post" accept-charset="utf-8">
 
 	<input type="hidden" name="MAX_FILE_SIZE" value="524288" />
 	
@@ -26,4 +43,91 @@
 	</fieldset>
 
 </form>
+
+<?php
+
+	 //Change permissions for all required folders (ex: img/products/category_name) to 777 first!!!
+ 
+ function processp(){
+ 
+ if (is_uploaded_file ($_FILES['image']['tmp_name'])){
+
+	$img=basename($_FILES['image']['name']);
+
+	$file = $_FILES['image'];
+
+	 
+		// Validate the file type:
+		$allowed_mime = array ('image/gif', 'image/pjpeg', 'image/jpeg', 'image/JPG', 'image/X-PNG', 'image/PNG', 'image/png', 'image/x-png');
+		$allowed_extensions = array ('.jpg', '.gif', '.png', 'jpeg');
+		$image_info = getimagesize($file['tmp_name']);
+		$ext = substr($file['name'], -4);
+		if ( (!in_array($file['type'], $allowed_mime)) 
+		||   (!in_array($image_info['mime'], $allowed_mime) ) 
+		||   (!in_array($ext, $allowed_extensions) ) 
+		) {
+			echo 'The uploaded file was not of the proper type. Must upload jpg, gif, png or jpeg';
+		} 	
+		else{
+	
+			$name = mysqli_real_escape_string($mysqli, $_POST['name']);
+			//($_FILES['image']['name'])
+			$cost = mysqli_real_escape_string($mysqli, $_POST['cost']);
+			$stock = mysqli_real_escape_string($mysqli, $_POST['stock']);
+			$description = mysqli_real_escape_string($mysqli, $_POST['description']);
+			$category = $_POST['category'];
+			
+			
+			//This is the directory where images will be saved
+			if ($category == 'Case Fan'){
+				$path ='../img/products/case-fan/'.$img;
+				$fpath='img/products/case-fan/'.$img;
+			}
+			else if ($category == 'Heatsink'){
+				$path ='../img/products/heatsink/'.$img;
+				$fpath='img/products/heatsink/'.$img;
+			}
+			else if ($category == 'Laptop Cooling'){
+				$path ='../img/products/laptopcooling/'.$img;
+				$fpath='img/products/laptopcooling/'.$img;
+			}
+			else if ($category == 'Water / Liquid Cooling'){
+				$path ='../img/products/liquid/'.$img;
+				$fpath='img/products/liquid/'.$img;
+			}
+			
+			//Prepped for query
+			$image = $fpath; 
+			
+				
+		//Writes the photo to the server
+	if(move_uploaded_file($_FILES['image']['tmp_name'], $path))
+	{
+
+	//Tells you if its all ok
+	echo "The file ". basename( $_FILES['image']['name']). " has been uploaded, and your information has been added to the directory";
+
+	$query="INSERT INTO products (product, description, category, stock, cost, image, rating) VALUES('$name','$description','$category','$stock','$cost','$image', '25')";
+		
+		$result=$mysqli->query($query)
+			or die ($mysqli->error);
+
+	}
+	else {
+
+	//Gives and error if its not
+	echo "Sorry, there was a problem uploading your file. ";
+	}
+	
+	}
+		//exit();
+		
+	}
+	else{echo 'You did not upload a file!';}
+	}
+	
+	
+	?>
+
+
 </div>
