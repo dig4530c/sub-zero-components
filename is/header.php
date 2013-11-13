1f61fc1a0d3efbff6369d9d3f5b69fe307874cce
Edgardo Jimenez <edjimenez87@gmail.com>
2013-11-12 21:13:17 -0500

    Header fixes.

--- is/header.php
+++ is/header.php
@@ -175,9 +175,8 @@ function copy_ship_info(f) {
 				<div id="hnav">
 					<ul>
 						<li><a href="catalog.php">Parts</a></li>
-						<li><a href="#">Brands</a></li>
-						<li><a href="#">Deals</a></li>
-						<li><a href="#">Trade</a></li>
+						<li><a href="brand.php">Brands</a></li>
+						<li><a href="deals.php">Deals</a></li>
 					</ul>
 					<div id="scontain">
 						<form id="search" method="get" action="search.php">
