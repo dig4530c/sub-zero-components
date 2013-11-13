904338db91d2db3e923a67c6e7ab68612c7a13cd
Edgardo Jimenez <edjimenez87@gmail.com>
2013-11-12 21:52:26 -0500

    More touchups.

--- background.php
+++ background.php
@@ -14,61 +14,58 @@ include ('is/dash.php');
 			<div class="tencol">
 				<div class='space'></div>
 				<div class='space'></div>
-				<div class="title login"> <!-- CHANGE -->
-					<h1>Company Background</h1>
+				<div class="title"> <!-- CHANGE -->
+					<h2>Company Background</h2>
 				</div>
-				<div id='login'> <!-- CHANGE -->
-					<h2 class="member">Members</h2>
-                    <br />
-                    <p class="space">
-					<span class="bold">Arissa Brown</span>
-					<br />Arissa Brown is the founder and CEO of SubZero Components. 
-					Arissa focuses her time on efficiently running the company and making sure customers are provided 
-					proper service. Her experience comes from years of study at the University of Central Florida and 
-					has proven experience in leading teams to great success.
-					</p>
-                    
-                    <p class="space">
-					<span class="bold">Katherine Delorme</span>
-					<br />Katherine Delorme handles all order processing. Katherine is a specialist in packing and shipping 
-					all of our products. From your order placement to delivery Katherine will be there for you making 
-					sure your order arrives safely and on-time. If you’re looking for someone to blame for a late 
-					delivery, Katherine’s you’re girl.
-                    </p>
-					
-                    <p class="space">
-					<span class="bold">Carlos A. Galindo</span>
-					<br />Carlos A. Galindo is our go-to guy for providing outstanding customer service. If you’re 
-					experiencing any problem placing your order, or would like some information about us, Carlos will 
-					be there to help. Carlos’ has proven work in customer service and will make sure your experience 
-					with SubZero is superb!
-					</p>
-                    
-                    <p class="space">
-					<span class="bold">Edgardo Jiménez</span>
-					<br />Edgardo Jimenez, the man behind the beast is our official web developer and IT freak. Should you 
-					experience problems on our site, it’s Edgardo’s fault; yet to happen. Edgardo joined the company 
-					in 2013 and produced our state-of-the-art website and checkout process. Edgardo brings efficiency 
-					to a whole new level at SubZero Components.
-					</p>
-                    
-                    <p class="space">
-					<span class="bold">James Schultz</span>
-					<br />James Schultz puts a nice touch on the madness with his first-class graphic design abilities. 
-					From our marketing collateral, print advertisements, promotional emails all the way to our website 
-					front-end, James graphic skills are unmatched in our industry. With over 3 years experience in 
-					several fields James’ artistic abilities are sure to please your eyes at any interaction with 
-					SubZero Components.
-					</p>
-                    
-                    <p class="space">
-					<span class="bold">Our Location</span>
-					<br />Corporate Headquarters
-					<br />4000 Central Florida Blvd
-					<br />Orlando, FL 32816
-					<br />(407) 823-2000
-                    </p>
-
+				<div> <!-- CHANGE -->
+					<h3 class="member">Members</h3>
+					<ul>
+						<li class="space">
+							<h4 class="bold">Arissa Brown</h4>
+							<p>Arissa Brown is the founder and CEO of SubZero Components. 
+							Arissa focuses her time on efficiently running the company and making sure customers are provided 
+							proper service. Her experience comes from years of study at the University of Central Florida and 
+							has proven experience in leading teams to great success.</p>
+						</li>
+						<li class="space">
+							<h4 class="bold">Katherine Delorme</h4>
+							<p>Katherine Delorme handles all order processing. Katherine is a specialist in packing and shipping 
+							all of our products. From your order placement to delivery Katherine will be there for you making 
+							sure your order arrives safely and on-time. If you’re looking for someone to blame for a late 
+							delivery, Katherine’s you’re girl.</p>
+						</li>
+						<li class="space">
+							<h4 class="bold">Carlos A. Galindo</h4>
+							<p>Carlos A. Galindo is our go-to guy for providing outstanding customer service. If you’re 
+							experiencing any problem placing your order, or would like some information about us, Carlos will 
+							be there to help. Carlos’ has proven work in customer service and will make sure your experience 
+							with SubZero is superb!</p>
+						</li>
+						<li class="space">
+							<h4 class="bold">Edgardo Jiménez</h4>
+							<p>Edgardo Jimenez, the man behind the beast is our official web developer and IT freak. Should you 
+							experience problems on our site, it’s Edgardo’s fault; yet to happen. Edgardo joined the company 
+							in 2013 and produced our state-of-the-art website and checkout process. Edgardo brings efficiency 
+							to a whole new level at SubZero Components.</p>
+						</li>
+						<li class="space">
+							<h4 class="bold">James Schultz</h4>
+							<p>James Schultz puts a nice touch on the madness with his first-class graphic design abilities. 
+							From our marketing collateral, print advertisements, promotional emails all the way to our website 
+							front-end, James graphic skills are unmatched in our industry. With over 3 years experience in 
+							several fields James’ artistic abilities are sure to please your eyes at any interaction with 
+							SubZero Components.</p>
+						</li>
+						<li class="space">
+							<h4 class="bold">Our Location</h4>
+							<ul>
+								<li>Corporate Headquarters</li>
+								<li>4000 Central Florida Blvd</li>
+								<li>Orlando, FL 32816</li>
+								<li>(407) 823-2000</li>
+							</ul>
+						</li>
+					</ul>
 				</div>
 			</div>
 			<div class="onecol last">
